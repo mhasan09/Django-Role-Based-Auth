5c067535b7865c63fae927b908ae65afe28d8ca3
Mahabubul Hasan <shourav6688@gmail.com>
2022-03-30 19:51:22 +0600

    user permission

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm, PostForm
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from .models import Post
 
 
@@ -12,12 +12,13 @@ def home(request):
         post_id = request.POST.get('post-id')
         print(post_id)
         post = Post.objects.filter(id=post_id).first()
-        if post and post.author == request.user:
+        if post and (post.author == request.user or request.user.has_perm('Authenticate.delete_post')):
             post.delete()
     return render(request, 'Pages/home.html', {'posts': POSTS})
 
 
 @login_required(login_url='/login')
+@permission_required('Authentication.add_post',login_url='/login', raise_exception=True)
 def create_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
